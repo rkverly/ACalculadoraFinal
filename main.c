17c40f74cd59c510430e0ea1f9edc31a70ef2542
ninguem <ninguem>
2023-06-13 16:38:59 -0300

    O programa recebe dois números como argumentos

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
 int main(){
+    float num1, num2;
+    float soma, subtracao, multiplicacao, divisao;
 
+    printf("Digite o primeiro número: ");
+    scanf("%f", &num1);
+
+    printf("\nDigite o segundo número: ");
+    scanf("%f", &num2);
 
 
 
